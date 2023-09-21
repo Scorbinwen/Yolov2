修复bug:network cls_score的softmax的维度有误，导致无法训练

config.py
@@ -11,14 +11,13 @@ ClsIdToName = \
      5: 'bus', 6: 'car', 7: 'cat', 8: 'chair', 9: 'cow',
      10: 'diningtable', 11: 'dog', 12: 'horse', 13: 'motorbike', 14: 'person',
      15: 'pottedplant', 16: 'sheep', 17: 'sofa', 18: 'train', 19: 'tvmonitor'}
-class_num = len(NameToClsId) + 1  # 1 denotes no object
+class_num = 2 + 1  # 1 denotes no object
 batch_size = 8
+test_batch_size = 1
 input_width = 416
 input_height = 416
-class_num = 20
 anchor_num = 4
 output_size = 13
-anchor_num = 4
 iou_threshold = 0.8
 scale_noobj = 0.006
 scale_obj = 1.0
@@ -36,7 +35,7 @@ anchor_train_epochs = 0
 # note that we assume the input_width equals to input_height
 downsample_rate = input_width // output_size
 loss_print_period = 10
-nms_iou_threshold = 0.5
+nms_iou_threshold = 0.7
 
 x = torch.arange(0, output_size, requires_grad=False, device="cuda")
 y = torch.arange(0, output_size, requires_grad=False, device="cuda")
@@ -48,10 +47,10 @@ path_to_state_dict = 'ModelPth\\state_dict_model.pth'
 loss_print_period = 10
 default_device = "cuda"
 tensorboard_logs = './logs'
-train_epochs = 5000
+train_epochs = 1500
 weight_decay = 0.0005
 momentum = 0.9
 
-dummy_lower_limit = 100
-dummy_upper_limit = 126
+dummy_lower_limit = 230
+dummy_upper_limit = 276
 dummy_dataset_len = 16

dataset.py
@@ -21,19 +21,20 @@ class DummyDataset(Dataset):
 
         target_cls = []
         target_object = []
-
-        pick_circle_x, pick_circle_y, pick_circle_size, iouflag = RandomGenerateBbox(target_object)
-        if iouflag == False:
-            cv2.circle(counters, (pick_circle_x, pick_circle_y), pick_circle_size // 2, (255, 255, 0), -1)
-            target_object.append(torch.tensor([pick_circle_x, pick_circle_y, pick_circle_size, pick_circle_size]))
-            target_cls.append(torch.tensor(0))
-
-        pick_rect_x, pick_rect_y, pick_rect_size, iouflag = RandomGenerateBbox(target_object)
-        if iouflag == False:
-            cv2.rectangle(counters, (pick_rect_x - pick_rect_size // 2, pick_rect_y - pick_rect_size // 2),
-                          (pick_rect_x + pick_rect_size // 2, pick_rect_y + pick_rect_size // 2), (125, 0, 125), -1)
-            target_object.append(torch.tensor([pick_rect_x, pick_rect_y, pick_rect_size, pick_rect_size]))
-            target_cls.append(torch.tensor(1))
+        rect_or_circle = random.randint(0, 1)
+        if (rect_or_circle == 0):
+            pick_circle_x, pick_circle_y, pick_circle_size, iouflag = RandomGenerateBbox(target_object)
+            if iouflag == False:
+                cv2.circle(counters, (pick_circle_x, pick_circle_y), pick_circle_size // 2, (255, 255, 0), -1)
+                target_object.append(torch.tensor([pick_circle_x, pick_circle_y, pick_circle_size, pick_circle_size]))
+                target_cls.append(torch.tensor(0))
+        else:
+            pick_rect_x, pick_rect_y, pick_rect_size, iouflag = RandomGenerateBbox(target_object)
+            if iouflag == False:
+                cv2.rectangle(counters, (pick_rect_x - pick_rect_size // 2, pick_rect_y - pick_rect_size // 2),
+                              (pick_rect_x + pick_rect_size // 2, pick_rect_y + pick_rect_size // 2), (125, 0, 125), -1)
+                target_object.append(torch.tensor([pick_rect_x, pick_rect_y, pick_rect_size, pick_rect_size]))
+                target_cls.append(torch.tensor(1))
 
         # transform image & target
         target_cls = torch.stack(target_cls)
@@ -70,7 +71,7 @@ def GetVOCDetectionDataLoader():
 
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size,
                                                    collate_fn=detection_collate, drop_last=True)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=config.batch_size,
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=config.test_batch_size,
                                                   collate_fn=detection_collate, drop_last=True)
     return train_dataloader, test_dataloader
 
@@ -83,6 +84,6 @@ def GetDummyDataDataLoader():
     # 数据迭代器
     train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=config.batch_size,
                                                    collate_fn=detection_collate, drop_last=True)
-    test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=config.batch_size,
+    test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=config.test_batch_size,
                                                    collate_fn=detection_collate, drop_last=True)
     return train_dataloader, test_dataloader

loss.py
@@ -79,7 +79,7 @@ class YoloLoss(nn.Module):
                                                                                               self.true_bbox[..., :4]))
 
         # class loss with no obj_mask, so that the network can learn no object.
-        score_loss = torch.mean(self.scale_obj * .5 * self.obj_mask[..., None] * self.mse_loss(cls_score, self.true_score))
+        score_loss = torch.mean(self.scale_obj * .5 * self.mse_loss(cls_score, self.true_score))
         # the loss may drop suddenly at iter 12800
         total_loss = (noobj_loss + obj_loss + prior_loss + true_loss + score_loss) / 4.
         return total_loss, noobj_loss / 4., obj_loss / 4., prior_loss / 4., true_loss / 4., score_loss / 4.

network.py
@@ -79,13 +79,13 @@ class DarkNet53(nn.Module):
         )
         self.cls_head = nn.Sequential(
             nn.Conv2d(1024, self.anchor_num * self.class_num, kernel_size=1, stride=1),
-            nn.Softmax(dim=-1),
         )
 
         self.reg_head = nn.Sequential(
             nn.Conv2d(1024, self.anchor_num * 5, kernel_size=1, stride=1),
         )
         self.sigmoid = torch.nn.Sigmoid()
+        self.softmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, x):
         h_52 = self.feature_52(x)
@@ -94,6 +94,7 @@ class DarkNet53(nn.Module):
 
         cls_score = self.cls_head(h_13)
         cls_score = torch.reshape(cls_score, (-1, self.fm_width, self.fm_height, self.anchor_num, self.class_num))
+        cls_score = self.softmax(cls_score)
         pred_object = self.reg_head(h_13)
         pred_object = torch.reshape(pred_object, (-1, self.fm_width, self.fm_height, self.anchor_num, 5))
 

train.py
@@ -3,6 +3,7 @@ import torchvision
 from loss import YoloLoss
 from network import *
 import config
+import signal
 from utils import *
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter(config.tensorboard_logs)
@@ -11,46 +12,74 @@ torch.set_default_device(config.default_device)
 from dataset import *
 train_dataloader, test_dataloader = GetDummyDataDataLoader()
 
-
 model = DarkNet53()
 criterion = YoloLoss()
 optimizer = torch.optim.RMSprop(params=model.parameters(), lr=config.learning_rate)
 
-if os.path.exists(config.path_to_state_dict):
-    print("loading model state dict...")
-    checkpoint = torch.load(config.path_to_state_dict)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-for epoch in range(config.train_epochs):
-    model.train()
-    for iter, (image, target) in enumerate(train_dataloader):
-        pred = model(image)
-        optimizer.zero_grad()
-        loss, noobj_loss, obj_loss, prior_loss, true_loss, score_loss = criterion(iter + epoch * len(train_dataloader), pred, target)
-        loss.backward()
-        optimizer.step()
-        if (iter + epoch * len(train_dataloader)) % config.loss_print_period == 0:
-            writer.add_scalar("loss", loss, iter + epoch * len(train_dataloader))
-            print("epoch:{} iter:{} total_loss:{}: noobj_loss:{}, obj_loss:{}, prior_loss:{}, true_loss:{}, score_loss:{}"
-                  "".format(epoch, iter, loss.item(), noobj_loss.item(), obj_loss.item(), prior_loss.item(), true_loss.item(), score_loss.item()))
-    # save state_dict every epoch
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': loss,
-    }, config.path_to_state_dict)
 
+def train():
+    if os.path.exists(config.path_to_state_dict):
+        print("loading model state dict...")
+        checkpoint = torch.load(config.path_to_state_dict)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    for epoch in range(config.train_epochs):
+        model.train()
+        for iter, (image, target) in enumerate(train_dataloader):
+            pred = model(image)
+            optimizer.zero_grad()
+            loss, noobj_loss, obj_loss, prior_loss, true_loss, score_loss = criterion(iter + epoch * len(train_dataloader), pred, target)
+            loss.backward()
+            optimizer.step()
+            if (iter + epoch * len(train_dataloader)) % config.loss_print_period == 0:
+                writer.add_scalar("loss", loss, iter + epoch * len(train_dataloader))
+                print("epoch:{} iter:{} total_loss:{}: noobj_loss:{}, obj_loss:{}, prior_loss:{}, true_loss:{}, score_loss:{}"
+                      "".format(epoch, iter, loss.item(), noobj_loss.item(), obj_loss.item(), prior_loss.item(), true_loss.item(), score_loss.item()))
+        # save state_dict every epoch.
+        s = signal.signal(signal.SIGINT, signal.SIG_IGN)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+        }, config.path_to_state_dict)
+        signal.signal(signal.SIGINT, s)
+
+        with torch.no_grad():
+            cls_score, pred_object = pred
+            cls_score_to_show = cls_score[0, ...]
+            image_to_show = image[0]
+            pred_object_to_show = pred_object[0, ...]
+            pred_object_to_show = MapPredCordBackToInputSize(pred_object_to_show)
+            pred_to_show = cls_score_to_show, pred_object_to_show
+            target = NMSbyConf(pred_to_show)
+            img = DrawWithPred(image_to_show, target)
+            # writer.add_image("pred_result", img, global_step=None, walltime=None, dataformats='CHW')
+            # ShowImageWbnd(img)
+    writer.close()
+
+
+def eval():
+    if os.path.exists(config.path_to_state_dict):
+        print("loading model state dict...")
+        checkpoint = torch.load(config.path_to_state_dict)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
     with torch.no_grad():
-        cls_score, pred_object = pred
-        cls_score_to_show = cls_score[0, ...]
-        image_to_show = image[0]
-        pred_object_to_show = pred_object[0, ...]
-        pred_object_to_show = MapPredCordBackToInputSize(pred_object_to_show)
-        pred_to_show = cls_score_to_show, pred_object_to_show
-        target = NMSbyConf(pred_to_show)
-        img = DrawWithPred(image_to_show, target)
-        writer.add_image("pred_result", img, 0, dataformats='CHW')
-        #ShowImageWbnd(img)
-writer.close()
+        for iter, (image, target) in enumerate(test_dataloader):
+                print("image shape", image.shape)
+                pred = model(image)
+                cls_score, pred_object = pred
+                cls_score_to_show = cls_score[0, ...]
+                image_to_show = image[0]
+                pred_object_to_show = pred_object[0, ...]
+                pred_object_to_show = MapPredCordBackToInputSize(pred_object_to_show)
+                pred_to_show = cls_score_to_show, pred_object_to_show
+                target = NMSbyConf(pred_to_show)
+                img = DrawWithPred(image_to_show, target)
+                # writer.add_image("pred_result", img, global_step=None, walltime=None, dataformats='CHW')
+                ShowImageWbnd(img)
+
+
+eval()